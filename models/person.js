00b0b25e7262b21a39dea51afd09ca3520d20609
”Joonas <joonas.kinn@gmail.com>
2019-10-08 21:48:09 +0300

    Fix bug

--- models/person.js
+++ models/person.js
@@ -1,6 +1,6 @@
 const mongoose = require('mongoose')
 
-const url = process.env.MONGODB_URI
+const url = 'mongodb+srv://joonaskinnunen:mvQuH1UTup8FX2Eo@cluster0-zq4nt.mongodb.net/phonebook-app?retryWrites=true&w=majority'
 
 console.log('connecting to', url)
 
